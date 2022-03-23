01f3560a6bfd22de92db27b81e6786650ddbd246
Vinicius <vinicius.savordelli@gmail.com>
2022-03-23 15:39:42 -0300

    feat: adds rate validation

--- middlewares/validations.js
+++ middlewares/validations.js
@@ -76,4 +76,13 @@ const validateAge = (req, res, next) => {
   next();
 };
 
+const validateRate = (req, res, next) => {
+  const { talk: { rate } } = req.body;
+  if (rate < 1 || rate > 5) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+
+next();  
+};
+
 module.exports = { validateEmail, validatePassword, validateAuth, validateName };
