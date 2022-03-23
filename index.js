f08f1cd6220411b3f9451f49e1a3d26828d28d3f
Vinicius <vinicius.savordelli@gmail.com>
2022-03-23 14:15:37 -0300

    feat:req2 adds the GET method to the path: /talker/:id allowing filtering talkers

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_NOT_FOUND_STATUS = 404;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -14,11 +15,26 @@ app.get('/', (_request, response) => {
 });
 
 app.get('/talker', async (_req, res) => {
-  const speaker = JSON.parse(await fs.readFile('./talker.json'));
+  const talkers = JSON.parse(await fs.readFile('./talker.json'));
 
-  if (!speaker) return res.status(HTTP_OK_STATUS).json([]);
+  if (!talkers) return res.status(HTTP_OK_STATUS).json([]);
 
-  return res.status(HTTP_OK_STATUS).json(speaker);
+  return res.status(HTTP_OK_STATUS).json(talkers);
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const allTalkers = JSON.parse(await fs.readFile('./talker.json'));
+
+  const talker = await allTalkers.find((currentTalker) => currentTalker.id === parseInt(id, 10));
+
+  if (!talker) {
+ return res.status(HTTP_NOT_FOUND_STATUS)
+  .json({ message: 'Pessoa palestrante não encontrada' }); 
+}
+
+  return res.status(200).json(talker);
 });
 
 app.listen(PORT, () => {
