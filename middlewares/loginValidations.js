c99b3eca079fae4d1f755fe8bff2ccccfd6c562e
Vinicius <vinicius.savordelli@gmail.com>
2022-03-23 14:42:31 -0300

    feat: add email and password validations as middlewares

---
+++ middlewares/loginValidations.js
@@ -0,0 +1,37 @@
+const validateEmail = (req, res, next) => {
+  // regex criado em outro projeto.
+  const REGEX = /[a-zA-Z0-9.-_]{1,}@[a-zA-Z.-]{2,}[.]{1}[a-zA-Z]{2,}/;
+  const { email } = req.body;
+
+  if (!email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+
+  if (!REGEX.test(email)) {
+    return res
+      .status(400)
+      .json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+
+  next();
+};
+
+const validatePassword = (req, res, next) => {
+  const { password } = req.body;
+
+  if (!password) {
+    return res
+      .status(400)
+      .json({ message: 'O campo "password" é obrigatório' });
+  }
+
+  if (password.length < 6) {
+    return res
+    .status(400)
+    .json({ message: 'O "password" deve ter pelo menos 6 caracteres' }); 
+  }
+
+  next();
+};
+
+module.exports = { validateEmail, validatePassword };
