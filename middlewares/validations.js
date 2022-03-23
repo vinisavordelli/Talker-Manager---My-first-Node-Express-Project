3610c4e5d68e614ac887d1189f5e635726b8eeec
Vinicius <vinicius.savordelli@gmail.com>
2022-03-23 15:04:43 -0300

    feat: add name validation

--- middlewares/validations.js
+++ middlewares/validations.js
@@ -46,4 +46,18 @@ const validateAuth = (req, res, next) => {
     next();
 };
 
-module.exports = { validateEmail, validatePassword };
+const validateName = (req, res, next) => {
+const { name } = req.body;
+
+if (!name) {
+  return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+}
+
+if (name.length < 3) {
+  return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+}
+
+next();
+};
+
+module.exports = { validateEmail, validatePassword, validateAuth, validateName };
